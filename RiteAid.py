ef7f1357de16b45b55ee8262a72636afa6d7eb37
Kunal Khosla <khosla.kunal@gmail.com>
2021-03-14 18:49:01 -0400

    Create RiteAid.py

---
+++ RiteAid.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+def findSlots(zipcode):
+    receive = requests.get(
+        'https://www.riteaid.com/services/ext/v2/stores/getStores?address=' + zipcode + '&attrFilter=PREF-112&fetchMechanismVersion=2&radius=50')
+    print(receive.text)
+    locationjson = json.loads(receive.text)
+    for stores in locationjson["Data"]["stores"]:
+        receive2 = requests.get(
+            'https://www.riteaid.com/services/ext/v2/vaccine/checkSlots?storeNumber=' + str(stores["storeNumber"]))
+        print(receive2.text)
+        slotdetails = json.loads(receive2.text)
+        if slotdetails["Data"]["slots"]["1"] is False & slotdetails["Data"]["slots"]["2"] is False:
+            print('No slots available')
+
+
+if __name__ == '__main__':
+    findSlots('07417')
