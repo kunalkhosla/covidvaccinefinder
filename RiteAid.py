774b370eea35d8dc19389acc48736cb8a25c6688
Kunal Khosla <khosla.kunal@gmail.com>
2021-03-14 21:46:17 -0400

    Update RiteAid.py

--- RiteAid.py
+++ RiteAid.py
@@ -1,19 +1,19 @@
 import requests
 import json
 
-def findSlots(zipcode):
+def findSlots(zipcode, radius):
     receive = requests.get(
-        'https://www.riteaid.com/services/ext/v2/stores/getStores?address=' + zipcode + '&attrFilter=PREF-112&fetchMechanismVersion=2&radius=50')
+        'https://www.riteaid.com/services/ext/v2/stores/getStores?address=' + zipcode + '&attrFilter=PREF-112&fetchMechanismVersion=2&radius=' + radius)
     print(receive.text)
     locationjson = json.loads(receive.text)
     for stores in locationjson["Data"]["stores"]:
         receive2 = requests.get(
             'https://www.riteaid.com/services/ext/v2/vaccine/checkSlots?storeNumber=' + str(stores["storeNumber"]))
-        print(receive2.text)
+        #print(receive2.text)
         slotdetails = json.loads(receive2.text)
         if slotdetails["Data"]["slots"]["1"] is False & slotdetails["Data"]["slots"]["2"] is False:
-            print('No slots available')
+            print('No slots available in ' + stores["address"])
 
 
 if __name__ == '__main__':
-    findSlots('07417')
+    findSlots('07417', '100')
